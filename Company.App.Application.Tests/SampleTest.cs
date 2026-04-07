df49b930401d54c88a9781d0ceb87cc2108f0542
Marius <142741541+MaRoeIT@users.noreply.github.com>
2026-04-08 00:53:11 +0200

    Added testingframework

---
+++ Company.App.Application.Tests/SampleTest.cs
@@ -0,0 +1,17 @@
+namespace Company.App.Application.Tests;
+
+public class SampleTest
+{
+    [Fact]
+    public void Sample_Test_Should_Pass()
+    {
+        // Arrange
+        var expected = true;
+
+        // Act
+        var actual = true;
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+}
