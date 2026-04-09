b32939beb2a23f972153117ec6b31e7a5e672c68
notveryhelpfuldude <caracass@hotmail.no>
2026-04-09 14:59:48 +0200

    Create MapMaterialDocumentationPackageDocument.cs

---
+++ Company.App.Application/UseCases/DataMapping/OneSubSea/MaterialDocumentationPackageDocumentMapper/MapMaterialDocumentationPackageDocument.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.App.Application.UseCases.DataMapping.OneSubSea.MaterialDocumentationPackageDocumentMapper
+{
+    internal class MapMaterialDocumentationPackageDocument
+    {
+
+    }
+}
