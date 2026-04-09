1b56aba28978c3f618464a7b2d2f90adb74b46ed
notveryhelpfuldude <caracass@hotmail.no>
2026-04-09 14:24:28 +0200

    Update MaterialDocumentationPackage.cs
    New strings for MDP header

--- Company.App.Domain/Entities/OneSubSea/MaterialDocumentationPackage.cs
+++ Company.App.Domain/Entities/OneSubSea/MaterialDocumentationPackage.cs
@@ -6,5 +6,17 @@
     /// </summary>
     public record MaterialDocumentationPackage : Order
     {
+        public MaterialDocumentationPackageHeader Header { get; } = new();
+
+    }
+    public record MaterialDocumentationPackageHeader
+    {
+        public string DocumentTitle { get; } = string.Empty;  
+        public string PurchaseOrderDate { get; } = string.Empty;
+        public string MDPIssueDateTime { get; } = string.Empty;
+        public string DocumentRevision { get; } = string.Empty;
+        public string OurReference { get; } = string.Empty;
+        public string BuyerTelephoneNumber { get; } = string.Empty;
+        public string Email { get; } = string.Empty;
     }
 }
