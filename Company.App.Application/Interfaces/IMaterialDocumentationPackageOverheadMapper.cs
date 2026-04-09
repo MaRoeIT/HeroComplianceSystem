40ebec18fc694cf3a0e328b575d83230b330f26b
notveryhelpfuldude <caracass@hotmail.no>
2026-04-09 15:38:55 +0200

    Create IMaterialDocumentationPackageOverheadMapper.cs
    made it public

---
+++ Company.App.Application/Interfaces/IMaterialDocumentationPackageOverheadMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.App.Application.Interfaces
+{
+    public interface IMaterialDocumentationPackageOverheadMapper
+    {
+    }
+}
