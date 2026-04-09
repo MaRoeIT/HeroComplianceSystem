da80e3c3296136f732a9acd06eaf1a858d97dffe
notveryhelpfuldude <caracass@hotmail.no>
2026-04-09 15:27:42 +0200

    Create IMaterialDocumentationPackageHeaderMapper.cs

---
+++ Company.App.Application/Interfaces/IMaterialDocumentationPackageHeaderMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company.App.Application.Interfaces
+{
+    internal interface IMaterialDocumentationPackageHeaderMapper
+    {
+    }
+}
