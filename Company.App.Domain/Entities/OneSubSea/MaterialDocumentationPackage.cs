088ff9e7fbe3287378efd1b779256a97f9174b41
notveryhelpfuldude <caracass@hotmail.no>
2026-04-13 12:39:57 +0200

    Update MaterialDocumentationPackage.cs

--- Company.App.Domain/Entities/OneSubSea/MaterialDocumentationPackage.cs
+++ Company.App.Domain/Entities/OneSubSea/MaterialDocumentationPackage.cs
@@ -7,6 +7,7 @@
     public record MaterialDocumentationPackage : Order
     {
         public MaterialDocumentationPackageHeader Header { get; } = new();
+        public MaterialDocumentationPackageIndex Index { get; } = new();
 
     }
     public record MaterialDocumentationPackageHeader
@@ -18,5 +19,50 @@
         public string OurReference { get; } = string.Empty;
         public string BuyerTelephoneNumber { get; } = string.Empty;
         public string Email { get; } = string.Empty;
+    }
+
+    public record MaterialDocumentationPackageIndex
+    {   
+        public List<MaterialMasterReport> MaterialMasterReports { get; } = new();
+
+    }
+    public record MaterialMasterReport
+    {
+        public string ReportDate{ get; } = string.Empty;
+        public string MaterialNumber { get; } = string.Empty;
+        public string Description { get; } = string.Empty;
+        public string RevisionLevel { get; } = string.Empty;
+        public string Plant { get; } = string.Empty;
+        public string BaseUnitOfMeasure { get; } = string.Empty;
+        public string GrossWeight { get; } = string.Empty;
+        public string NetWeight { get; } = string.Empty;
+        public string BasicMaterial { get; } = string.Empty;
+        public string CommodityCode { get; } = string.Empty;
+        public string ExportControlCode { get; } = string.Empty;
+        public string Manufacturer { get; } = string.Empty;
+        public string ManufacturerPartNumber { get; } = string.Empty;
+        public string MIRID { get; } = string.Empty;
+        public string MIRTitle { get; } = string.Empty;
+        public string MIRDescription { get; } = string.Empty;
+        public string Criticality { get; } = string.Empty;
+        public string TraceabilityType { get; } = string.Empty;
+        public string SerialNumberProfile { get; } = string.Empty;
+        public string ShelfLife { get; } = string.Empty;
+        public string QMControlKey { get; } = string.Empty;
+        public List<InspectionSetup> InspectionSetups { get; } = new List<InspectionSetup>();
+        public List<Classification> Classifications { get; } = new List<Classification>();
+        public List<OtherRelatedDoc> OtherRelatedDocs { get; } = new List<OtherRelatedDoc>();
+    }
+    public record InspectionSetup
+    {
+        public string InspectionSetupItem { get; } = string.Empty;
+    }
+    public record Classification
+    {
+        public string ClassificationItem { get; } = string.Empty;
+    }
+    public record OtherRelatedDoc 
+    {
+        public string OtherRelatedDocItem { get; } = string.Empty;
     }
 }
