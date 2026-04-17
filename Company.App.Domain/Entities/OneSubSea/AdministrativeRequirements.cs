af7aa7a539738eb8177989ff43e1acb400789fc8
Andreas Isaksen <andreis@hiof.no>
2026-04-17 10:32:58 +0200

    AdminReq domain start.
    Basic structure for AdministrativeRequirements class in domain.

--- Company.App.Domain/Entities/OneSubSea/AdministrativeRequirements.cs
+++ Company.App.Domain/Entities/OneSubSea/AdministrativeRequirements.cs
@@ -6,9 +6,81 @@
     /// </summary>
     public record AdministrativeRequirements : Order
     {
+        public AdministrativeRequirementsHeader Header { get; }
+
+        public AdministrativeRequirementsOverHead OverHead { get; }
+
         public AdministrativeRequirements(
+            AdministrativeRequirementsHeader header,
+            AdministrativeRequirementsOverHead overHead,
             string orderNumber,
             string orderDate
-            ) : base(orderNumber, orderDate) { }
+            ) : base(orderNumber, orderDate)
+        {
+            Header = header;
+            OverHead = overHead;
+        }
+    }
+
+    public record AdministrativeRequirementsHeader
+    {
+        public string DocumentId { get; }
+
+        public string RevisionNumber { get; }
+
+        public DateOnly IssueDate { get; }
+        
+        public string BusinessSegment { get; }
+
+        public string BusinessProcess { get; }
+
+        public string Owner { get; }
+        
+        public string Approver { get; }
+        
+        public string Author { get; }
+    }
+
+    public record AdministrativeRequirementsOverHead
+    {
+        public HashSet<int> NumberOfPages { get; }
+
+        public HashSet<int> NumberOfDocuments { get; }
+
+        public IReadOnlyList<string> DocumentList { get; }
+
+        public AdministrativeRequirementsOverHead(
+            HashSet<int> numberOfPages,
+            HashSet<int> numberOfDocuments,
+            IReadOnlyList<string> documentList)
+        {
+            NumberOfPages = numberOfPages;
+            NumberOfPages = numberOfPages;
+            DocumentList = documentList;
+        }
+    }
+
+    public record AdministrativeRequirementsSection
+    {
+        public string Title { get; }
+
+        public HashSet<int> Pages { get; }
+
+        public IReadOnlyList<string> Content { get; }
+
+        public IReadOnlyList<AdministrativeRequirementsSection> SubSections { get; }
+
+        public AdministrativeRequirementsSection(
+            string title,
+            HashSet<int> pages,
+            IReadOnlyList<string> content,
+            IReadOnlyList<AdministrativeRequirementsSection> subSections
+            )
+        {
+            Title = title;
+            Pages = pages;
+            Content = content;
+            SubSections = subSections;
+        }
     }
 }
